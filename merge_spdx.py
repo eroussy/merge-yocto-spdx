6ba0e0a009151b5210d68063dc232b0ab8fc8627
Erwann Roussy <erwann.roussy@savoirfairelinux.com>
2023-07-26 15:14:27 +0200

    merge_spdx.py: add logs and comments

--- merge_spdx.py
+++ merge_spdx.py
@@ -11,24 +11,23 @@ from spdx_tools.spdx.writer.write_anything import write_file
 def parse_arguments():
     parser = argparse.ArgumentParser(description="SPDX Directory and Output Parser")
 
-    # Add arguments for SPDX directory, main file, and output file.
     parser.add_argument(
         "-d", "--spdx-directory",
         type=str,
         required=True,
-        help="The directory containing SPDX JSON files.",
+        help="The directory containing SPDX files. Only .spdx.json file will be analyzed.",
     )
     parser.add_argument(
         "-m", "--main-file",
         type=str,
         required=True,
-        help="The main SPDX JSON file.",
+        help="The main SPDX file. It will be used as core document, all other packages will be added to it.",
     )
     parser.add_argument(
         "-o", "--output",
         type=str,
         required=True,
-        help="The output SPDX filename.",
+        help="The output file. It can be a json file or spdx file depending of the extension you choose.",
     )
 
     args = parser.parse_args()
@@ -45,20 +44,17 @@ def list_files_in_directory(directory):
     return file_list
 
 spdx_files = list_files_in_directory(args.spdx_directory)
-print("LOG : get json files OK")
 
 main_doc = parse_file(args.main_file)
 
 for f in spdx_files:
-    print("doing {}".format(f))
+    print("processing {}".format(f))
     try:
         doc = parse_file(f)
         main_doc.packages += doc.packages
     except:
-        print("error reading : {}".format(f))
+        print("error reading : {} Drop the file and continue.".format(f))
         continue
 
-print("LOG : parse json files OK")
-
-# write file anyway
+# Write file
 write_file(main_doc, args.output, validate=False)
